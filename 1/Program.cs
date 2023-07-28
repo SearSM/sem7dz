92cc15e5e7fb66622f245ef0bb8cf1669f6a49c5
Sergei <MrSearSe@gmail.com>
2023-07-28 10:40:58 +0500

    complete 1 task

---
+++ 1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void PrintDoubleMassive (double[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]:F1} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+double[,] MakeDoubleMassive(int rows, int columns)
+{
+    double[,] massive = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            massive[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return massive;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInfo("Введите количество строк: ");
+int columns = GetInfo("Введите количество столбцов: ");
+double[,] massive = MakeDoubleMassive(rows, columns);
+PrintDoubleMassive(massive);
